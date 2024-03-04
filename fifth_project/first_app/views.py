9d5e1d6b6f5bfd2c7e414fce1c3dcd457a4efe8f
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-03-05 00:31:27 +0600

    Module 13

---
+++ fifth_project/first_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home (request):
+    return render(request, './first_app/home.html')
+
+def about (request):
+    if request.method == 'POST':
+        print(request.POST)
+        name = request.POST.get('username')
+        email = request.POST.get('email')
+        select = request.POST.get('select')
+        return render(request, './first_app/about.html', {'name': name, 'email': email, 'select': select})
+    else:
+        return render(request, './first_app/about.html')
+
+def submit_form (request):
+    # print(request.POST)
+    
+    return render(request, './first_app/form.html')

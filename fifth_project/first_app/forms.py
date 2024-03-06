ce9de3904219cbb3d7096aba5134b93291bd766b
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-03-07 02:40:32 +0600

    day 164

--- fifth_project/first_app/forms.py
+++ fifth_project/first_app/forms.py
@@ -1,11 +1,29 @@
 from django import forms
 
 class contactForm(forms.Form):
-    name = forms.CharField(label='User Name')
+    name = forms.CharField(label='User Name', help_text='Total length must be within 50 Characters', required=False, widget = forms.Textarea(attrs={'id' : 'text_area', 'class' : 'class1 class2', 'placeholder' : 'Enter your name'}))
+    # files = forms.FileField()
     email = forms.EmailField(label='User Email')
-    age = forms.IntegerField(label='Age') 
-    weight = forms.FloatField(label='Weight')
-    balance = forms.DecimalField(label='Balance')
+    # age = forms.IntegerField(label='Age') 
+    # weight = forms.FloatField(label='Weight')
+    # balance = forms.DecimalField(label='Balance')
+    age = forms.CharField(widget=forms.NumberInput)
     check = forms.BooleanField(label='Check')
-    birthday = forms.DateField(label='Birthday')
-    appointment = forms.DateTimeField(label='Appointment')
+    # birthday = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
+    birthday = forms.CharField(widget=forms.DateInput(attrs={'type' : 'date'}))
+    # appointment = forms.DateTimeField(widget=forms.DateInput(attrs={'type' : 'datetime-local'}))
+    appointment = forms.CharField(widget=forms.DateInput(attrs={'type' : 'datetime-local'}))
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    MEAL = [('P', 'Pepperoni'), ('M', 'Mashroom'), ('B', 'Beef')]
+    pizza = forms.MultipleChoiceField(choices=MEAL, widget=forms.CheckboxSelectMultiple)
+    
+
+class studentData(forms.Form):
+    name = forms.CharField(widget=forms.TextInput)
+    email = forms.CharField(widget=forms.EmailInput)
+    def clean_name(self):
+        valname = self.cleaned_data['name']
+        if len(valname) < 10:
+            raise forms.ValidationError('Name must be greater than 10')
+        return valname
--- fifth_project/first_app/views.py
+++ fifth_project/first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . forms import contactForm
+from . forms import contactForm, studentData
 # Create your views here.
 def home (request):
     return render(request, './first_app/home.html')
@@ -20,7 +20,24 @@ def submit_form (request):
     return render(request, './first_app/form.html')
 
 def DjangoForms(request):
-    form = contactForm(request.POST)
-    if form.is_valid():
-        print(form.cleaned_data)
+    if request.method == 'POST':
+        form = contactForm(request.POST, request.FILES)
+        if form.is_valid():
+            # files = form.cleaned_data['files']
+            # with open('./first_app/upload/' + files.name, 'wb+') as destination:
+            #     for chunk in files.chunks():
+            #         destination.write(chunk)
+            print(form.cleaned_data)
+            return render (request, './first_app/django_form.html', {'form' : form})
+    else:
+        form = contactForm()
+    return render (request, './first_app/django_form.html', {'form' : form})
+
+def studentForm(request):
+    if request.method == 'POST':
+        form = studentData(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = studentData()
     return render (request, './first_app/django_form.html', {'form' : form})

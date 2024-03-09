4418dd0d1d39e594b31cfa3ee312fbec974af6df
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-03-09 23:43:25 +0600

    Module 14 Codes

---
+++ Module 14 Models and Modelform in Django/seventh_project/first_app/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from first_app.models import StudentModel
+# Create your forms here.
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = StudentModel
+        fields = '__all__'
+        # exclude = ['roll']
+        labels = {
+            'name' : 'Student Name',
+            'roll' : 'Student Roll'
+        }
+        widgets = {
+            'name' : forms.TextInput(attrs = {'class' : 'btn btn-primary'}),
+            'roll' : forms.PasswordInput()
+        }
+        help_texts = {
+            'name' : 'Enter Student Name',
+            'roll' : 'Enter Student Roll'
+        }
+        error_messages = {
+            'name' : {'required': 'Your name is required'}
+        }

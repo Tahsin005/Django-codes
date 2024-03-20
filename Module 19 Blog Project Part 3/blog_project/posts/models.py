8b9a0109188def3435d994d3398d7dd24195cb91
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-03-20 15:02:22 +0600

    Module 19 Codes

--- Module 19 Blog Project Part 3/blog_project/posts/models.py
+++ Module 19 Blog Project Part 3/blog_project/posts/models.py
@@ -11,3 +11,15 @@ class Post(models.Model):
     image = models.ImageField(upload_to='posts/media/uploads/', blank=True, null=True)
     def __str__(self) -> str:
         return self.title
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True) # The time when an object is created
+    
+    def __str__(self):
+        return f'Comments by {self.name}'
+    

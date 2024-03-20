db0aae6257389233f64adf9261f37e90613772f9
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-03-20 06:04:32 +0600

    Module 19 Codes

---
+++ Module 19 Blog Project Part 3/blog_project/author/views.py
@@ -0,0 +1,107 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from author.forms import RegistrationForm, ChangeUserForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from posts.models import Post
+from django.contrib.auth.views import LoginView, LogoutView
+
+from django.views.generic import UpdateView
+from django.contrib.auth.models import User
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        register_form = RegistrationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, 'Registration successful')
+            return redirect('register')
+    else:
+        register_form = RegistrationForm()
+    return render(request,'register.html', {'form': register_form, 'type' : 'Register'})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data['username']
+            user_pass = form.cleaned_data['password']
+            user = authenticate(username=user_name, password=user_pass)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Logged in successfully')
+                return redirect('profile')
+            else:
+                messages.warning(request, 'Login information is incorrect')
+                return redirect('login')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'register.html', {'form': form, 'type' : 'Login'})
+
+
+class UserLoginView(LoginView):
+    template_name = 'register.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('profile')
+    def form_valid(self, form):
+        messages.success(self.request, 'Logged in successfully')
+        return super().form_valid(form)
+    def form_invalid(self, form):
+        messages.success(self.request, 'Logged in information incorrect')
+        return super().form_invalid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'Login'
+        return context
+
+@login_required            
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully')
+    return redirect('user_login')
+@login_required            
+def profile(request):
+    data = Post.objects.filter(author = request.user)
+    return render(request,'profile.html' , {'data': data})
+
+@login_required            
+def edit_profile(request):
+    if request.method == 'POST':
+        profile_form = ChangeUserForm(request.POST, instance=request.user)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+            return redirect('profile')
+    else:
+        profile_form = ChangeUserForm(instance=request.user)
+    return render(request,'update_profile.html', {'form': profile_form})
+
+# Edit profile using login view
+class EditProfileView(UpdateView):
+    form_class = ChangeUserForm
+    template_name = 'update_profile.html'
+    success_url = reverse_lazy('profile')
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+@login_required            
+def pass_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, data=request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            messages.success(request, 'Password changed successfully')
+            update_session_auth_hash(request, form.user)
+            return redirect('profile')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'pass_change.html', {'form': form})
+
---
+++ Module 19 Blog Project Part 3/blog_project/posts/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render, redirect
+from posts.forms import PostForm
+from . import models
+from posts.models import Post
+from django.contrib.auth.decorators import login_required
+
+
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy
+
+from django.utils.decorators import method_decorator
+
+# Create your views here.
+@login_required
+def add_post(request):
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # form.cleaned_data['author'] = request.user
+            form.instance.author = request.user
+            form.save()
+            print(form.cleaned_data)
+            return redirect('add_post')
+    return render(request, 'add_post.html', {'form': form})
+
+# Add post using class based view
+@method_decorator(login_required, name='dispatch')
+class AddPostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    success_url = reverse_lazy('add_post')
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    
+@login_required
+def edit_post(request, id):
+    post = models.Post.objects.get(pk = id)
+    form = PostForm(instance=post)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.instance.author = request.user
+            form.save()
+            print(form.cleaned_data)
+            return redirect('homepage')
+    return render(request, 'add_post.html', {'form': form})
+
+# Edit post using class based view
+@method_decorator(login_required, name='dispatch')
+class EditPostView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('profile')
+    
+
+@login_required
+def delete_post(request, id):
+    post = models.Post.objects.get(pk = id)
+    post.delete()
+    return redirect('profile')
+
+# Delete post using class based view
+@method_decorator(login_required, name='dispatch')
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete.html'
+    success_url = reverse_lazy('profile')
+    pk_url_kwarg = 'id'
+    
+    
+class DetailPostView(DetailView):
+    model = Post
+    pk_url_kwarg = 'id'
+    template_name = 'post_details.html'

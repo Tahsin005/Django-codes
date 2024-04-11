b9fa69dc65102181a1d51e304c129226e8b3cae6
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-11 14:22:02 +0600

    gs49 - model form - labels, help_text, error_messages, widgets

---
+++ A_yeeet/gs49/enroll/forms.py
@@ -0,0 +1,39 @@
+from django import forms 
+from django.core import validators
+from . models import User
+
+class StudentRegistration(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+        labels = {
+            'name' : 'Enter your name',
+            'email' : 'Enter your email address',
+            'password' : 'Enter your password'
+        }
+        help_text = {
+            'name' : 'Enter your full name',
+            'email' : 'Enter your email address',
+            'password' : 'Enter your password',
+        }
+        error_messages = {
+            'name' : {
+               'required' : 'Name is required',
+            },
+            'email' : {
+               'required' : 'Email is required',
+            },
+            'password' : {
+               'required' : 'Password is required',
+            }
+        }
+        widgets = {
+            'password' : forms.PasswordInput(attrs={
+                'placeholder' : 'Password dede bhai',
+            }),
+            'name' : forms.TextInput(attrs={
+                'class' : 'myclass',
+                'placeholder' : 'Yaha naam likh'
+            })
+        }
+        

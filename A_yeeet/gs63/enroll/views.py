66ade7de570f3e7f66e0c11219aeabe5152913ae
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-15 00:32:35 +0600

    gs63

---
+++ A_yeeet/gs63/enroll/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from . froms import SignUpForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+def sign_up(request):
+    if request.method == 'POST':
+        fm = SignUpForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            messages.success(request, 'Your account has been created successfully')
+    else:
+        fm = SignUpForm()
+    return render(request, 'enroll/signup.html', {'form': fm})
+
+def userlogin(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/profile/')
+    else:
+        if request.method == 'POST':
+            fm = AuthenticationForm(request=request, data=request.POST)
+            if fm.is_valid():
+                uname = fm.cleaned_data['username']
+                upass = fm.cleaned_data['password']
+                
+                user = authenticate(username=uname, password=upass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Logged in successfully')
+                    return HttpResponseRedirect('/profile/')
+        else:
+            fm = AuthenticationForm()
+        return render(request, 'enroll/userlogin.html', {'form':fm})
+
+def userprofile(request):
+    if request.user.is_authenticated:
+        return render(request, 'enroll/profile.html', {'name' : request.user})
+    else:
+        return HttpResponseRedirect('/login/')
+        
+
+def userlogout(request):
+    return HttpResponseRedirect('/login/')

00d4177631061902ac4bb847235f955393c0538e
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-18 23:19:23 +0600

    gs82

--- A_yeeet/gs82/enroll/views.py
+++ A_yeeet/gs82/enroll/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 # Create your views here.
 
-@cache_page(60)
 def home(request):
     return render(request, 'enroll/course.html')
 
--- A_yeeet/gs82/gs82/urls.py
+++ A_yeeet/gs82/gs82/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from enroll import views
+from django.views.decorators.cache import cache_page
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', cache_page(30)(views.home)),
+    path('home/', views.home),
     path('contact', views.contacts),
 ]

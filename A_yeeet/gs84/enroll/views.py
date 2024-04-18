62913ada2a145cbc62504542657dc93ae48708d2
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-18 23:33:33 +0600

    gs84

---
+++ A_yeeet/gs84/enroll/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+# Create your views here.
+
+def home(request):
+    return render(request, 'enroll/course.html')
+
+def contacts(request):
+    return render(request, 'enroll/contact.html')

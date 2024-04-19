e1d53edeadbdb1eed815faa387f2efe21788b05c
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-19 23:26:24 +0600

    gs89 - manual connect - signal:

---
+++ A_yeeet/gs89/blog/signals.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.contrib.auth.models import User
+
+
+def login_success(sender, request, user, **kwargs):
+    print('--------------------------------')
+    print('Logged in signal......')
+    print('Sender : ', sender)
+    print('Request : ', request)
+    print('User : ', user)
+    print('User Password : ', user.password)
+    print(f'kwargs : {kwargs}')
+    
+user_logged_in.connect(login_success, sender=User)

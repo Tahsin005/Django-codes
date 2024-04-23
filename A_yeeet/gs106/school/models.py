6d0216e76b282e4caf3135a6739093eeeb76743d
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-24 00:03:36 +0600

    gs104 & 105

--- A_yeeet/gs106/school/models.py
+++ A_yeeet/gs106/school/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from . managers import CustomManager
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=70)
     roll = models.IntegerField()
-    
-    students = models.Manager()
+    objects = models.Manager()
+    students = CustomManager()

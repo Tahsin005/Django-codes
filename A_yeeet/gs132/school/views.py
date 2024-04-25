091a9e1b832b9841e8672354982ed3c5e65b17f1
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-04-25 22:38:53 +0600

    gs132

---
+++ A_yeeet/gs132/school/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from . models import Student
+from django.views.generic.base import TemplateView
+from . forms import StudnetForm
+# Create your views here.
+
+class StudentCreateView(CreateView):
+    form_class = StudnetForm
+    template_name ='school/student_form.html'
+    success_url = '/thanks/'
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudnetForm
+    template_name ='school/student_form.html'
+    success_url = '/thanksupdate/'
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    template_name = 'school/student_delete.html'
+    success_url = '/create/'
+
+class ThanksTemplateView(TemplateView):
+    template_name = 'school/thanks.html'
+    
+class ThanksDeleteTemplateView(TemplateView):
+    template_name = 'school/thanksdelete.html'
+    
+class ThanksUpdateTemplateView(TemplateView):
+    template_name = 'school/thanks_update.html'
+
+    

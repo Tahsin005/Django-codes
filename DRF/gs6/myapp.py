d6940b56df005d495110ae4302550ce530e12688
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-15 02:17:36 +0600

    gs6 - model serializers

---
+++ DRF/gs6/myapp.py
@@ -0,0 +1,60 @@
+import requests
+import json
+
+URL= "http://127.0.0.1:8000/studentapi/"
+
+def get_data(id = None):
+    data = {}
+    if id is not None:
+        data = {'id' : id}
+    json_data = json.dumps(data)
+    r = requests.get(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+
+def post_data():
+    data = {
+        'name' : 'morzia',
+        'roll' : 125,
+        'city' : 'dhaka'
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.post(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+    
+    
+def update_data():
+    data = {
+        'id' : 5,
+        'name' : 'Ferdous',
+        'city' : 'coxs bazar'
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.put(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+    
+def delete_data():
+    data = {
+        'id' : 5,
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.delete(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+
+delete_data()
+# update_data() 
+# post_data()
+# get_data(1)
+    
+    
+    
+    

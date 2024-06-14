e3c77ad2253a634848b907d163f32c439a2c647c
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-15 02:35:50 +0600

    gs7 - special attribute on the serializers fields

---
+++ DRF/gs7/myapp.py
@@ -0,0 +1,61 @@
+import requests
+import json
+
+URL= "http://127.0.0.1:8000/studentapi/"
+
+def get_data(id = None):
+    data = {}
+    if id is not None:
+        data = {'id' : id}
+    json_data = json.dumps(data)
+    r = requests.get(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+
+def post_data():
+    data = {
+        'name' : 'morzia',
+        'roll' : 125,
+        'city' : 'dhaka'
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.post(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+    
+    
+def update_data():
+    data = {
+        'id' : 3,
+        'name' : 'Cubox',
+        'roll' : 70,
+        'city' : 'nice'
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.put(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+    
+def delete_data():
+    data = {
+        'id' : 6,
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.delete(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+
+# delete_data()
+update_data() 
+# post_data()
+# get_data()
+    
+    
+    
+    

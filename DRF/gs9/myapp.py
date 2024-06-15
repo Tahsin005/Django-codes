39ca79b159cf47d12811c1bcd8ee8bb98fab13a3
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-15 12:13:48 +0600

    function based api view

---
+++ DRF/gs9/myapp.py
@@ -0,0 +1,72 @@
+import requests
+import json
+
+URL= "http://127.0.0.1:8000/studentapi/"
+
+def get_data(id = None):
+    data = {}
+    if id is not None:
+        data = {'id' : id}
+    headers = {
+        'content-Type' : 'application/json'
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.get(url = URL, headers = headers, data = json_data)
+    data = r.json()
+    print(data)
+
+def post_data():
+    data = {
+        'name' : 'mohit',
+        'roll' : 190,
+        'city' : 'yyy'
+    }
+    headers = {
+        'content-Type' : 'application/json'
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.post(url = URL, headers = headers, data = json_data)
+    data = r.json()
+    print(data)
+    
+    
+def update_data():
+    data = {
+        'id' : 3,
+        'name' : 'Cubox',
+        'roll' : 70,
+        'city' : 'nice'
+    }
+    headers = {
+        'content-Type' : 'application/json'
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.put(url = URL, headers = headers, data = json_data)
+    data = r.json()
+    print(data)
+    
+def delete_data():
+    data = {
+        'id' : 6,
+    }
+    
+    json_data = json.dumps(data)
+    
+    r = requests.delete(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+
+# delete_data()
+# update_data() 
+post_data()
+get_data()
+    
+    
+    
+    

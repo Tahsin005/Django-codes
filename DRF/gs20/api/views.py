daa50ba48c911cf55f9f3ba597feddd0e7353028
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-21 16:01:15 +0600

    gs20 - global settings for permission classes

--- DRF/gs20/api/views.py
+++ DRF/gs20/api/views.py
@@ -4,10 +4,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]

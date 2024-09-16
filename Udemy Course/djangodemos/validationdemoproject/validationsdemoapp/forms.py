cf03512a197b0df1f323419423f99e8fd372bf67
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-16 22:14:01 +0600

    implementing clean method for individual fields

--- Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/forms.py
+++ Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/forms.py
@@ -1,6 +1,6 @@
 from . models import UserRegistration
 from django import forms 
-
+import re
 class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = UserRegistration
@@ -26,3 +26,12 @@ class UserRegistrationForm(forms.ModelForm):
             'email': forms.EmailInput(),
             'website_url': forms.URLInput(),
         }
+    # Field level validation
+    def clean_phone_number(self):
+        iphonenumber = self.cleaned_data.get('phone_number')
+        if iphonenumber:
+            pattern = re.compile(r"(0|91)?[6-9][0-9]{9}")
+            if not re.fullmatch(pattern, iphonenumber):
+                raise forms.ValidationError("Invalid Phone Number! Example: 919234567891")
+            return iphonenumber
+        

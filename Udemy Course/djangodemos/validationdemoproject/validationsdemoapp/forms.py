93c01c59b9d402409b2812162012a446849c9673
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-16 19:10:38 +0600

    Using Django Built In Form Widgets like RadioButton,DropDownList,CheckBox

--- Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/forms.py
+++ Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/forms.py
@@ -5,3 +5,24 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = UserRegistration
         fields = '__all__'
+        
+        genders = [{'male', 'Male'}, {'females', 'Female'}]
+        countries = [
+            ("select", "Please Choose Country"),
+            ('Bangladesh', 'Bangladesh'),
+            ('USA', 'USA'),
+            ('UK', 'UK'),
+            ('Germany', 'Germany'),
+            ('France', 'France'),
+            ('Italy', 'Italy'),
+        ]
+        
+        widgets = {
+            'password': forms.PasswordInput(),
+            'confirm_password': forms.PasswordInput(),
+            'gender': forms.RadioSelect(choices=genders),
+            'country': forms.Select(choices=countries),
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),
+            'email': forms.EmailInput(),
+            'website_url': forms.URLInput(),
+        }

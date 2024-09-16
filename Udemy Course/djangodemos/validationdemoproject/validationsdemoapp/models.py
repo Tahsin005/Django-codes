41fc229c2fc4eb9166b55c0a8f33009e608c8c02
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-16 21:56:43 +0600

    implementing length validators

--- Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/models.py
+++ Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/models.py
@@ -1,17 +1,21 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 # Create your models here.
 class UserRegistration(models.Model):
-    username = models.CharField(max_length=15, verbose_name="User Name")
-    password = models.CharField(max_length=15, verbose_name="Password")
-    confirm_password = models.CharField(max_length=15, verbose_name="Confirm Password")
+    # username = models.CharField(max_length=15, verbose_name="User Name")
+    username = models.CharField(max_length=15, verbose_name="User Name", validators=[MinLengthValidator(5)])
+    # password = models.CharField(max_length=15, verbose_name="Password")
+    password = models.CharField(max_length=15, verbose_name="Password", validators=[MinLengthValidator(5)])
+    # confirm_password = models.CharField(max_length=15, verbose_name="Confirm Password")
+    confirm_password = models.CharField(max_length=15, verbose_name="Confirm Password", validators=[MinLengthValidator(5)])
     
     gender = models.CharField(max_length=10, verbose_name="Gender")
     country = models.CharField(max_length=20, verbose_name="Country")
     date_of_birth = models.DateField(verbose_name="Date of Birth")
     
     email = models.EmailField(verbose_name="Email")
-    postal_code = models.IntegerField(verbose_name="Postal Code")
+    # postal_code = models.IntegerField(verbose_name="Postal Code")
+    postal_code = models.IntegerField(verbose_name="Postal Code", validators=[MinValueValidator(1000), MaxValueValidator(9999999)])
     phone_number = models.CharField(max_length=13, verbose_name="Phone Number")
     
     profile = models.TextField(verbose_name="Profile of User", blank=True)
--- Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/views.py
+++ Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/views.py
@@ -4,4 +4,9 @@ from . forms import UserRegistrationForm
 
 def SignUp(request):
     form = UserRegistrationForm()
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            # form.save()
+            return render(request, "validationsdemoapp/Home.html")
     return render(request, "validationsdemoapp/SignUp.html", {'form': form})

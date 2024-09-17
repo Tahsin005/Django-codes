9b2b508ea36cc559e801181cf2fc208e91325e55
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-17 22:46:53 +0600

    Rendering custom error messages

--- Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/models.py
+++ Udemy Course/djangodemos/validationdemoproject/validationsdemoapp/models.py
@@ -12,9 +12,9 @@ def validate_favwebsiteurl(iurl):
 # Create your models here.
 class UserRegistration(models.Model):
     # username = models.CharField(max_length=15, verbose_name="User Name")
-    username = models.CharField(max_length=15, verbose_name="User Name", validators=[MinLengthValidator(5)])
+    username = models.CharField(max_length=15, verbose_name="User Name", validators=[MinLengthValidator(5, message="Min length must be greater than 5")])
     # password = models.CharField(max_length=15, verbose_name="Password")
-    password = models.CharField(max_length=15, verbose_name="Password", validators=[MinLengthValidator(5)])
+    password = models.CharField(max_length=15, verbose_name="Password", validators=[MinLengthValidator(5, message="Min length must be greater 5")])
     # confirm_password = models.CharField(max_length=15, verbose_name="Confirm Password")
     confirm_password = models.CharField(max_length=15, verbose_name="Confirm Password", validators=[MinLengthValidator(5)])
     
@@ -24,7 +24,7 @@ class UserRegistration(models.Model):
     
     email = models.EmailField(verbose_name="Email")
     # postal_code = models.IntegerField(verbose_name="Postal Code")
-    postal_code = models.IntegerField(verbose_name="Postal Code", validators=[MinValueValidator(1000), MaxValueValidator(9999999)])
+    postal_code = models.IntegerField(verbose_name="Postal Code", validators=[MinValueValidator(1000, message="Postal code must be greater than 1000"), MaxValueValidator(9999999, message="Postal code must be less that 999999")])
     phone_number = models.CharField(max_length=13, verbose_name="Phone Number")
     
     profile = models.TextField(verbose_name="Profile of User", blank=True)

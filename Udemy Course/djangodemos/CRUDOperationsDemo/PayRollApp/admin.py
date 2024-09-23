6d84ee96d7074fbb5a08684f888403f48bba8ea2
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-23 19:19:40 +0600

    seeding data to database

--- Udemy Course/djangodemos/CRUDOperationsDemo/PayRollApp/admin.py
+++ Udemy Course/djangodemos/CRUDOperationsDemo/PayRollApp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from . models import Employee, PartTimeEmployee
+from . models import Employee, PartTimeEmployee, OnSiteEmployees, State, City
 # Register your models here.
 admin.site.register(Employee)
 admin.site.register(PartTimeEmployee)
+admin.site.register(OnSiteEmployees)
+admin.site.register(City)
+admin.site.register(State)

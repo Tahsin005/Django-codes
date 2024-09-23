d567a32e0cee48cddf06c7d8ef7940a3dd7ca08d
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-23 19:29:39 +0600

    creating a formModel class and setting custom attributes to all form fields using widgets

--- Udemy Course/djangodemos/CRUDOperationsDemo/PayRollApp/forms.py
+++ Udemy Course/djangodemos/CRUDOperationsDemo/PayRollApp/forms.py
@@ -1,5 +1,5 @@
-from django import forms 
-from . models import Employee, PartTimeEmployee
+from django import forms
+from . models import Employee, PartTimeEmployee, OnSiteEmployees
 
 class EmployeeForm(forms.ModelForm):
     class Meta:
@@ -27,3 +27,38 @@ class NewPartTimeEmployeeForm(forms.ModelForm):
         fields = '__all__'
         
 PartTimeEmployeeFormSet = forms.modelformset_factory(PartTimeEmployee, form=NewPartTimeEmployeeForm, extra=10)
+
+
+
+class OnSiteEmployeesForm(forms.ModelForm):
+    class Meta:
+        model = OnSiteEmployees
+        fields = ['first_name', 'last_name', 'country', 'state', 'city']
+        
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'style': 'max-width: 300px;',
+                'placeholder': 'First Name'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'style': 'max-width: 300px;',
+                'placeholder': 'Last Name'
+            }),
+            'country': forms.Select(attrs={
+                'class': 'form-control',
+                'style': 'max-width: 300px;',
+                'placeholder': 'Country'
+            }), 
+            'state': forms.Select(attrs={
+                'class': 'form-control',
+                'style': 'max-width: 300px;',
+                'placeholder': 'State'
+            }),
+            'city': forms.Select(attrs={
+                'class': 'form-control',
+                'style': 'max-width: 300px;',
+                'placeholder': 'City'
+            }),
+        }

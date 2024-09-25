c9283b1891bc15d000bb4d57847b901802e9e931
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-25 10:08:12 +0600

    accessing generated models using django ORM

--- Udemy Course/djangodemos/dbfirstapproachdemo/dbfirstapproachapp/views.py
+++ Udemy Course/djangodemos/dbfirstapproachdemo/dbfirstapproachapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . models import Categories
 # Create your views here.
+def ShowCategories(request):
+    categories = Categories.objects.all()
+    
+    return render(request, 'dfap/ShowCategories.html', {'categories': categories})

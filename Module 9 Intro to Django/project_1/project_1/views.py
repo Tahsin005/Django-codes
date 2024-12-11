c6daf2e1b75904fe77b01de57a1c4547f5c79e1a
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-12-11 18:35:18 +0600

    edit

--- Module 9 Intro to Django/project_1/project_1/views.py
+++ Module 9 Intro to Django/project_1/project_1/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def home (request):
-    return HttpResponse("Hello, world. You're at the home page.")
+    return HttpResponse("Hello, world. You're at the home page. Welcome Tahsin.")
 def contact (request):
     # pass
     return HttpResponse("Hello, world. You're at the contact page.")
